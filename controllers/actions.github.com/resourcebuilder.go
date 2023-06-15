24603c3791eece2fa7985d2ecbe75f42e7035c17
Nikola Jokic <jokicnikola07@gmail.com>
2023-06-15 13:01:06 +0200

    Update queue duration, move totals scaleset-based to statistic

--- controllers/actions.github.com/resourcebuilder.go
+++ controllers/actions.github.com/resourcebuilder.go
@@ -224,6 +224,7 @@ func (b *resourceBuilder) newScaleSetListenerPod(autoscalingListener *v1alpha1.A
 			corev1.ContainerPort{
 				ContainerPort: int32(port),
 				Protocol:      corev1.ProtocolTCP,
+				Name:          "metrics",
 			},
 		)
 	}
